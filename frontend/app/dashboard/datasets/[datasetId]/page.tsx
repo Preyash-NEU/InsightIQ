a6aa78d1b3e08560f8b2380c8c254cea5960594c
Preyash Amar Mehta <mehta.prey@northeastern.edu>
2025-11-09 00:48:18 -0500

    Remove duplicate dataset detail route

--- frontend/app/dashboard/datasets/[datasetId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DatasetDetailView } from "./dataset-detail-view";
-
-export default function DatasetDetailPage({
-  params,
-}: {
-  params: { datasetId: string };
-}) {
-  return <DatasetDetailView datasetId={decodeURIComponent(params.datasetId)} />;
-}
